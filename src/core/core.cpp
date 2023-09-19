fc4b3ca67c080946aa56788734cb71d6a92b07a1
turbohedgehog <unlinker@mail.ru>
2023-09-19 22:09:06 +0300

    Core integration

---
+++ src/core/core.cpp
@@ -0,0 +1,38 @@
+#include "core/core.h"
+
+#include <iostream>
+
+namespace m2d {
+
+Core::Core() {}
+
+Core& Core::GetInstance() {
+  static Core core;
+
+  return core;
+}
+
+int Core::Run(int argc, char* argv[]) {
+  if (status_ != Core::Status::Stopped) {
+    return 1;
+  }
+
+  status_ = Core::Status::Starting;
+  core_config_.Parse(argc, argv);
+
+  status_ = Core::Status::Running;
+
+  std::cout << "Monstrum2D!";
+
+  status_ = Core::Status::Stopping;
+
+  status_ = Core::Status::Stopped;
+
+  return 0;
+}
+
+Core::Status Core::GetStatus() const {
+  return status_;
+}
+
+}  // namespace m2d
---
+++ src/core/core.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+
+#include "core/core_config.h"
+
+namespace m2d {
+
+class Core {
+ public:
+  enum class Status {
+    Stopped,
+    Starting,
+    Running,
+    Stopping,
+  };
+
+  static Core& GetInstance();
+
+  int Run(int argc, char* argv[]);
+  Status GetStatus() const;
+ 
+ private:
+  Core();
+
+  Status status_ = Status::Stopped;
+  CoreConfig core_config_;
+};
+
+}  // namespace m2d
