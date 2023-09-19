47a51ba7c805d21206647ab3d78b5faaa1fcd224
turbohedgehog <unlinker@mail.ru>
2023-09-19 22:56:34 +0300

    Impl #4

---
+++ src/core/core_config.cpp
@@ -0,0 +1,25 @@
+#include "core_config.h"
+
+#include <iostream>
+
+#include <boost/program_options.hpp>
+
+namespace m2d {
+
+void CoreConfig::Parse(int argc, char* argv[]) {
+  namespace po = boost::program_options;
+  po::options_description desc("Options");
+  desc.add_options()
+    ("help,h", "Show help")
+    ;
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
+
+  if (vm.count("help")) {
+    std::cout << desc << "\n";
+  }
+}
+
+}  // namespace m2d
